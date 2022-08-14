7953863e76360f72cb498bc4c37bf37e38a6f356
Canberk Aydemir <canberk.aydemir@yahoo.com>
2022-08-14 13:16:30 +0300

    Build the foundation of  each  microservice

--- cards/src/main/java/com/cards/CardsApplication.java
+++ cards/src/main/java/com/cards/CardsApplication.java
@@ -2,8 +2,15 @@ package com.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScans({ @ComponentScan("com.cards.controller") })
+@EnableJpaRepositories("com.cards.repository")
+@EntityScan("com.cards.model")
 public class CardsApplication {
 
 	public static void main(String[] args) {
